82dfc81e94b00b371ebd2d9a3b6c06772d5d3da7
Dmitry Baryshev <dmitrymq@gmail.com>
2020-07-22 15:01:03 +0300

    Version update to 0.9.0-pre6

--- Formula/sail.rb
+++ Formula/sail.rb
@@ -5,6 +5,12 @@ class Sail < Formula
   sha256 "b2b6e9bc67cb9f69cf92bd084c2bb01d2d6ce9740592a7ace7966c36453c0887"
   license "MIT"
 
+  bottle do
+    root_url "https://dl.bintray.com/smoked-herring/bottles-sail"
+    sha256 "46e4212a601e0f1f90b327f4b192f518ac7a2d1ca51984134d08b1be00a2f999" => :catalina
+    sha256 "7109320af74e663321025db089d0802a42ca166537c10aa3aed2e0af4ea371c1" => :mojave
+  end
+
   option "with-sail-dev", "Enable development features like pedantic warnings and ASAN (if possible)"
 
   depends_on "cmake" => :build
