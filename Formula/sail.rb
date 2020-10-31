897ba9c868cc7a09367a832ceeefa0b799336e15
Dmitry Baryshev <dmitrymq@gmail.com>
2020-10-31 23:39:42 +0300

    Remove bottles before updating them to 0.9.0-pre9

--- Formula/sail.rb
+++ Formula/sail.rb
@@ -1,15 +1,9 @@
 class Sail < Formula
   desc "Fast and lightweight cross-platform image decoding and encoding library"
   homepage "https://github.com/smoked-herring/sail"
-  url "https://github.com/smoked-herring/sail/archive/v0.9.0-pre8.tar.gz"
-  sha256 "f8738d436a6933879e8ec83c46795cbf587b4009a25f6049062ad62333d9589f"
+  url "https://github.com/smoked-herring/sail/archive/v0.9.0-pre9.tar.gz"
+  sha256 "b49dcad863ff8c1990766ab594022c382f0cb7b733ca1bb4bddc27e58d2424ee"
   license "MIT"
-
-  bottle do
-    root_url "https://dl.bintray.com/smoked-herring/bottles-sail"
-    sha256 "f4ef2269e03ad3120e7f9b0dc449dfb30918b626d64e6c0656e8957ee20a0281" => :catalina
-    sha256 "1268ff9d9a6a2f1cac38ef1fe317115342c5d1809dc439d9b885eaf5569999f4" => :mojave
-  end
 
   option "with-sail-dev", "Enable development features like pedantic warnings and ASAN (if possible)"
 
