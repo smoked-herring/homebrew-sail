7bf743305f2ed7b17c1b107f457e75b4179b2396
Dmitry Baryshev <dmitrymq@gmail.com>
2020-12-18 14:18:33 +0300

    Update to e72578a

--- Formula/sail.rb
+++ Formula/sail.rb
@@ -1,8 +1,9 @@
 class Sail < Formula
   desc "Fast and lightweight cross-platform image decoding and encoding library"
   homepage "https://github.com/smoked-herring/sail"
-  url "https://github.com/smoked-herring/sail/archive/v0.9.0-pre11.tar.gz"
-  sha256 "764ff648e1711dce3b2c2007b069df6652ee45a6ababef731f5e1580277ce5c6"
+  url "https://github.com/smoked-herring/sail/archive/e72578a.tar.gz"
+  version "0.9.0-pre11"
+  sha256 "c2bd067232959b5a72a7ed65d7c2d420c3bfec9aa99c3741ecaa7346f03251fc"
   license "MIT"
 
   option "with-sail-dev", "Enable development features like pedantic warnings and ASAN (if possible)"
